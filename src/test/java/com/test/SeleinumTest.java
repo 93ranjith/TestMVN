39afa46b6234c036ad6cde7aa634de3cf19154c6
93ranjith <kumaresanravirani@gmail.com>
2022-06-15 09:17:13 +0530

    aurn work done

--- src/test/java/com/test/SeleinumTest.java
+++ src/test/java/com/test/SeleinumTest.java
@@ -3,4 +3,14 @@ package com.test;
 public class SeleinumTest {
 	
 
+	   private void arun() {
+		   
+		System.out.println(" this is arun file ");
+		
+	}
+	   public static void main(String[] args) {
+		
+		   SeleinumTest st =new SeleinumTest();
+		   st.arun();
+	}
 }
